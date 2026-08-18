6deffde737ea76dc85c284d272be154311cbc0ca
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2026-08-18 16:08:43 -0700

    Enable force LB

--- nemo_rl/models/megatron/setup.py
+++ nemo_rl/models/megatron/setup.py
@@ -904,6 +904,10 @@ def _apply_moe_config(model_cfg: Any, config: PolicyConfig) -> None:
     model_cfg.moe_token_dispatcher_type = config["megatron_cfg"][
         "moe_token_dispatcher_type"
     ]
+    if "moe_router_force_load_balancing" in config["megatron_cfg"]:
+        model_cfg.moe_router_force_load_balancing = config["megatron_cfg"][
+            "moe_router_force_load_balancing"
+        ]
     if "inference_moe_token_dispatcher_type" in config["megatron_cfg"]:
         model_cfg.inference_moe_token_dispatcher_type = config["megatron_cfg"][
             "inference_moe_token_dispatcher_type"
